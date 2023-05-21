edb21b8d9b9653f12fae062775b245d3c0575239
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-05-21 15:20:15 +0800

    add lsp support for typescript and javascript

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -44,6 +44,10 @@ return {
 					"rust-analyzer",
 				},
 			})
+      lsp.tsserver.setup {
+        on_attach = on_attach,
+        capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+      }
 		end,
 	},
 }
