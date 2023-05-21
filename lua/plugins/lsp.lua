0e1ae4b040a83cfd5d7381aae2879a67a34d8cc2
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-05-21 15:28:06 +0800

    add lsp support for sql

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -48,6 +48,10 @@ return {
         on_attach = on_attach,
         capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
       }
+      lsp.sqlls.setup{
+        on_attach = on_attach,
+      }
+
 		end,
 	},
 }
