816856c148d3551d6913318968b8cb4e1cb7c3a5
hadihammurabi <hadihammurabi@gmail.com>
2023-08-09 11:44:40 +0700

    cmp: tab and shift-tab to select auto complete

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line -1, line, true)[1] : sub(col, col) : match('%s') == nil
+end
+
 return {
 	-- {
 	-- 	'tzachar/cmp-tabnine',
@@ -41,6 +47,24 @@ return {
 						select = true,
 					}),
 					["<C-Space>"] = cmp.mapping.complete(),
+          ["<Tab>"] = function(fallback)
+            if not cmp.select_next_item() then
+              if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                cmp.complete()
+              else
+                fallback()
+              end
+            end
+          end,
+          ["<S-Tab>"] = function(fallback)
+            if not cmp.select_prev_item() then
+              if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                cmp.complete()
+              else
+                fallback()
+              end
+            end
+          end,
 				}),
 
 				snippet = {
