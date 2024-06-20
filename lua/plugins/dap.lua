f753bc9d1282bdc33388c028c76bcf7dca0b36a2
hadihammurabi <hadihammurabi@gmail.com>
2024-06-20 20:27:40 +0700

    refactor: vim dap go at outside

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -5,13 +5,11 @@ return {
       "mfussenegger/nvim-dap",
       "williamboman/mason.nvim",
       "jay-babu/mason-nvim-dap.nvim",
-      "leoluz/nvim-dap-go",
       "theHamsta/nvim-dap-virtual-text",
       "nvim-neotest/nvim-nio",
     },
     config = function()
       require("mason-nvim-dap").setup({})
-      require('dap-go').setup()
       require("nvim-dap-virtual-text").setup()
 
       local dap, dapui = require("dap"), require("dapui")
@@ -27,6 +25,17 @@ return {
 
       -- vim.cmd("DapLoadLaunchJSON")
       require('dapui').setup()
+    end,
+  },
+
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      require('dap-go').setup()
     end,
   },
 
