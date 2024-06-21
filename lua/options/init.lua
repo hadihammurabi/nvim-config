0b3b4770922d8748956959351fe076b6377ca1ba
hadihammurabi <hadihammurabi@gmail.com>
2024-06-21 12:37:38 +0700

    mouse only in visual mode

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -5,11 +5,15 @@ vim.opt.number = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.list = false
-vim.opt.mouse = c
--- vim.opt.listchars:append "eol:¬"
+vim.opt.mouse = 'v'
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
 vim.opt.syntax = 'enable'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+-- vim.opt.listchars:append "eol:¬"
 
 vim.g.mapleader = ' '
 
@@ -23,5 +27,4 @@ vim.g.netrw_sizestyle = "H"
 
 vim.g.rnvimr_enable_ex = 1
 vim.g.rnvimr_enable_picker = 1
-vim.g.rnvimr_ranger_cmd = {'ranger', '--cmd=set draw_borders both'}
-
+vim.g.rnvimr_ranger_cmd = { 'ranger', '--cmd=set draw_borders both' }
