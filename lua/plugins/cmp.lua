ef729c919520909f50a6ace402abaf49b8b11c23
hadihammurabi <hadihammurabi@gmail.com>
2025-01-28 00:41:58 +0700

    fix(cmp): configure for better completion experience

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -12,6 +12,24 @@ return {
       sources = {
         default = { "lsp", "snippets", "path", "buffer" },
       },
+      completion = {
+        ghost_text = {
+          enabled = true,
+        },
+        menu = {
+          draw = {
+            columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "source_name" } },
+          }
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 0,
+          treesitter_highlighting = false,
+        },
+        list = {
+          selection = { preselect = true, auto_insert = false },
+        },
+      },
     },
   },
 }
