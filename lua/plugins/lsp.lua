844722128c8a6ea1a3a5fe41195c152effb62ec5
hadihammurabi <hadihammurabi@gmail.com>
2025-01-29 10:18:16 +0700

    refactor: move flutter into LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -72,4 +72,43 @@ return {
       })
     end
   },
+  {
+    'akinsho/flutter-tools.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim',
+      "nvim-telescope/telescope.nvim",
+    },
+    ft = { "dart" },
+    config = function()
+      require("telescope").load_extension("flutter")
+      require("flutter-tools").setup {
+        ui = {
+          border = "rounded",
+        },
+        decorations = {
+          statusline = {
+            app_version = true,
+            device = true,
+          },
+        },
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
+        },
+        widget_guides = {
+          enabled = true,
+        },
+        closing_tags = {
+          enabled = true,
+        },
+        dev_log = {
+          enabled = true,
+        },
+        lsp = {
+          enabled = false,
+        }
+      }
+    end,
+  }
 }
